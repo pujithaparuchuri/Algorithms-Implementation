1cae934814bf755ebb0d3884848ea226e9b3a90b
SRI PARUCHURI <sriparuchuri2@gmail.com>
2018-04-14 20:19:00 -0400

    Add files via upload

---
+++ Sorting Algorithms Implementation/src/com/sort/BubbleSortImpl.java
@@ -0,0 +1,41 @@
+package com.sort;
+
+public class BubbleSortImpl {
+	  
+    // logic to sort the elements
+    public static void bubble_srt(int array[]) {
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    swapNumbers(i, k, array);
+                }
+            }
+            printNumbers(array);
+        }
+    }
+  
+    private static void swapNumbers(int i, int j, int[] array) {
+  
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  
+    private static void printNumbers(int[] input) {
+          
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + ", ");
+        }
+        System.out.println("\n");
+    }
+  
+    public static void main(String[] args) {
+        int[] input = { 45,23,1,67,44,3,76,54,34,22,12,13};
+        bubble_srt(input);
+  
+    }
+}
